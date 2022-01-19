7e69842f43a1c051ed7656df22cf4c0b24e27bc9
aKharkov <aKharkov@mail.ru>
2022-01-19 23:42:31 +0300

    add modal win to newsList and vueX

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 const bodyParser = require('body-parser')
 const express = require('express')
 const fs = require('fs');
@@ -17,6 +22,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var tmp_routers = require("./tmp_routes.js");
 
 //================================ Разрешение CORS ============================================
 app.use(function (req, res, next) {
@@ -41,10 +49,14 @@ const urlencodedParser = bodyParser.urlencoded({
 
 //include("/js/block_mongo.js");
 const { MongoClient } = require("mongodb");
-const mongoose = require('mongoose');
+ mongoose = require('mongoose'); //убрал локализующее объявление CONST
 const { errorMonitor } = require('events');
 const { Console } = require('console');
 const { Schema } = mongoose;
+
+
+
+
 
 //============================== Роуты start ===============================
 app.get('/', function (request, response) {
@@ -73,17 +85,17 @@ app.get('/products', function (request, response) {
 })
 
 app.get("/--", function(req, res)  {
-    get2minus(ProductCards, res);
+    tmp_routers.get2minus(ProductCards, res);
 });
 
 /* эта функция просто создает N карточек продуктов..... */
 app.get("/----", function(req, res)  {
-    get2plus(ProductCards, res);
+    tmp_routers.get2plus(ProductCards, res);
 });
 /* эта функция просто создает N карточек продуктов..... */
 
 app.get("/addnews", function(req, res)  {
-    get2news(News, res);
+    tmp_routers.get2news(News, res);
 
 });
 //============================== Роуты end ===============================
@@ -131,6 +143,8 @@ async function run() {
         process.exit(1)
     }
 }
+
+
 
 run().catch(console.dir);
 
@@ -204,11 +218,12 @@ const schemaNews = new mongoose.Schema({
 
 );
 
-const productType = mongoose.model('productType', schemaProductType);
-const ProductCards = mongoose.model('productCards', schemaProductCard);
-const News = mongoose.model('News', schemaNews);
 
 
+//убрал локализующее объявление CONST
+ productType = mongoose.model('productType', schemaProductType);
+ ProductCards = mongoose.model('productCards', schemaProductCard);
+ News = mongoose.model('News', schemaNews);
 
 
 
@@ -234,269 +249,5 @@ const News = mongoose.model('News', schemaNews);
 
 
 
-// -- начало -- эта функция просто создает N карточек продуктов.....  техническая функция вне проекта
-async function get2plus(collect, res){
 
-    let docCount=0;
 
-    const prodTypes = await productType.find();
-    console.log('prodTypes=',prodTypes); //[0]._id
-
-    /* 
-    prodTypes.forEach(item => {
-      console.log('prodTypes===',item._id);
-    }); 
-    
-    */
-
-
-    await ProductCards.find()
-        .exec(function(err, Product_Cards) {
-
-            /* console.log('Product_Cards==',Product_Cards);
-            console.log('Product_Cards.length==',Product_Cards.length  );
-
-            console.log('ProductCards.countDocuments==',ProductCards.countDocuments() );
-            console.log('Product_Cards.countDocuments==',Product_Cards.proto ); */
-//
-            //ProductCards.estimatedDocumentCount(function (err, count) {
-
-            if (err){
-                console.log(err)
-            }else{
-
-                docCount = 0; //count;
-                
-                console.log("Estimated Count docCount= :", docCount);
-
-                if (docCount===0) {
-                    let prodCard;
-
-                    var id ;
-                    let prodTypeNum=0;
-                    let picNo=0;
-                  
-
-
-
-
-
-                    for (let i=1;i<51;i++) {
-
-                        (picNo<6) ? picNo++ : picNo=0;
-                        (prodTypeNum<1) ? prodTypeNum++ : prodTypeNum=0;
- 
-
-
-// id = ObjectId('619ea5069365ba31cc27cfe0');
-// id = mongoose.Types.ObjectId('619ea5069365ba31cc27cfe0');  
-// console.log('mongoose.Types.ObjectId(  ===  ',id);
-
-
-
-
-                        prodCard = new ProductCards( {
-                            _id: new mongoose.Types.ObjectId(),
-                            productType: prodTypes[prodTypeNum]._id
-                            , //код типа продукта из справочника
-                            productName: 'Продукт № '+i, //название продукта
-                            productProp: 'String'+i,  // свойства продукта
-                            pic: 'Pic'+ picNo,  // ()=>{(picNo<6) ? (picNo++, return picNo ) : (picNo=0,return picNo )} //ссылка на файл изображения
-                            productCost: 10*i, //цена
-                            productEnable: 1,
-                            productPromo: 1
-
-                        });
-
-                        
-
-
-                        prodCard.save()
-                            .then(function(doc){
-                                console.log("Сохранен объект", doc);
-
-                            })
-                            .catch(function (err){
-                                console.log('Ошибка!!!!!!!!!!!!!!!!!!!!!!!!!!   ',err);
-
-                            });
-                    }
-                }
-
-
-                ProductCards.find({ productName: /кофе/i  }).limit(10)
-                    .exec( function (err, small) {
-                        if (err){
-                            console.log('Erroo = ',err);
-                            return err;}
-                        else{
-                            res.status(200).send(small); //json
-                            console.log("Данные отправленны",small);
-                            //ProductCards.collection.drop();
-                        }
-                    });
-
-
-            }
-            //});
-
-            //console.log(ProductCardss);
-        });
-
-
-
-
-};
-// -- конец -- эта функция просто создает N карточек продуктов.....  техническая функция вне проекта
-
-// -- начало -- эта функция просто создает N карточек типов продуктов.....  техническая функция вне проекта
-async function get2minus(collect, res){
-    let docCount;
-    await ProductCards.find({}).exec(function(err, Product_Cards) {
-        console.log(Product_Cards);
-        Product_Cards.estimatedDocumentCount(function (err, count) {
-
-            if (err){
-                console.log(err)
-            }else{
-
-                docCount = count;
-                console.log("Estimated Count docCount= :", docCount);
-
-                if (docCount===0) {
-
-                    let prodType = new productType( {
-                        _id: new mongoose.Types.ObjectId(),
-                        productType: '0000000001',
-                        productName: 'Кофе',
-                        productTypeEnable: 0
-
-                    });
-
-                    prodType.save()
-                        .then(function(doc){
-                            console.log("Сохранен объект", doc);
-
-                        })
-                        .catch(function (err){
-                            console.log('Ошибка!!!!!!!!!!!!!!!!!!!!!!!!!!   ',err);
-
-                        });
-
-                }
-
-
-                /*
-                               Book.find({
-                                 title: /mvc/i
-                             }).sort('-created')
-                             .limit(5)
-                             .exec(function(err, books) {
-                                 if (err) throw err;
-
-                                 console.log(books);
-                             }); */
-                //.create
-                /*
-                                      _id: new mongoose.Types.ObjectId(),
-                                      coffeeName: `Какой-то кофе № ${docCount}`,
-                                      coffeeRegion: new mongoose.Types.ObjectId(),
-                                      detail: `Много много интересного`,
-                                      pic: "Картинка",
-                                      created:   new Date()
-                   */
-
-                ProductCards.find({
-                    productName: /кофе/i
-                }).limit(10)
-                    .exec( function (err, small) {
-                        if (err){
-                            console.log('Erroo = ',err);
-                            return err;}
-                        else{
-
-
-                            res.status(200).send(small); //json
-                            console.log("Данные отправленны",small);
-                            //ProductCards.collection.drop();
-
-                        }
-                    });
-
-
-            }
-        });
-        //console.log(ProductCardss);
-    });
-};
-// -- конец -- эта функция просто создает N карточек типов продуктов.....  техническая функция вне проекта
-
-
-// -- начало -- эта функция просто создает N карточек новостей.....  техническая функция вне проекта
-async function get2news(collect, res){
-    let docCount;
-    await News.find({}).exec(function(err, News_Cards) {
-        console.log(collect);
-        collect.estimatedDocumentCount(function (err, count) {
-
-            if (err){
-                console.log(err)
-            }else{
-
-                docCount = count;
-                console.log("Estimated Count docCount News= :", docCount);
-const fillText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-
-
-                if (docCount===0) {
-
-                    let newsCard;
-                    for (i=1;i<11;i++) {
-                        newsCard = new collect( {
-                            _id: new mongoose.Types.ObjectId(),
-                            newsTitle: "Новость № "+i,  //код типа продукта
-                            newsBody: i+") Много интересного "+fillText,
-                            newsEnable: 1,
-                            newsDateBegin: new Date()
-                            // newsDateEnd: Date
-
-                        });
-                        newsCard.save()
-                            .then(function(doc){
-                                console.log("Сохранен объект", doc);
-
-                            })
-                            .catch(function (err){
-                                console.log('Ошибка!!!!!!!!!!!!!!!!!!!!!!!!!!   ',err);
-
-                            });
-                    }
-                }
-
-
-
-                News.find({
-                    newsTitle: /кофе/i
-                }).limit(10)
-                    .exec( function (err, small) {
-                        if (err){
-                            console.log('Erroo = ',err);
-                            return err;}
-                        else{
-
-
-                            res.status(200).send(small); //json
-                            console.log("Данные отправленны",small);
-                            //ProductCards.collection.drop();
-
-                        }
-                    });
-
-
-            }
-        });
-        //console.log(ProductCardss);
-    });
-};
-// -- конец -- эта функция просто создает N карточек типов продуктов.....  техническая функция вне проекта
---
+++ server/tmp_routes.js
@@ -0,0 +1,278 @@
+
+
+
+
+// -- начало -- эта функция просто создает N карточек продуктов.....  техническая функция вне проекта
+async function get2plus(collect, res){
+
+    let docCount=0;
+
+    const prodTypes = await productType.find();
+    console.log('prodTypes=',prodTypes); //[0]._id
+
+    /* 
+    prodTypes.forEach(item => {
+      console.log('prodTypes===',item._id);
+    }); 
+    
+    */
+
+
+    await ProductCards.find()
+        .exec(function(err, Product_Cards) {
+
+            /* console.log('Product_Cards==',Product_Cards);
+            console.log('Product_Cards.length==',Product_Cards.length  );
+
+            console.log('ProductCards.countDocuments==',ProductCards.countDocuments() );
+            console.log('Product_Cards.countDocuments==',Product_Cards.proto ); */
+//
+            //ProductCards.estimatedDocumentCount(function (err, count) {
+
+            if (err){
+                console.log(err)
+            }else{
+
+                docCount = 0; //count;
+                
+                console.log("Estimated Count docCount= :", docCount);
+
+                if (docCount===0) {
+                    let prodCard;
+
+                    var id ;
+                    let prodTypeNum=0;
+                    let picNo=0;
+                  
+
+
+
+
+
+                    for (let i=1;i<51;i++) {
+
+                        //(picNo<6) ? picNo++ : picNo=0;
+                        (prodTypeNum<1) ? prodTypeNum++ : prodTypeNum=0;
+ 
+
+
+// id = ObjectId('619ea5069365ba31cc27cfe0');
+// id = mongoose.Types.ObjectId('619ea5069365ba31cc27cfe0');  
+// console.log('mongoose.Types.ObjectId(  ===  ',id);
+
+
+
+
+                        prodCard = new ProductCards( {
+                            _id: new mongoose.Types.ObjectId(),
+                            productType: prodTypes[prodTypeNum]._id
+                            , //код типа продукта из справочника
+                            productName: 'Продукт № '+i, //название продукта
+                            productProp: 'String'+i,  // свойства продукта
+                            pic:  () => {picNo<6 ? 'Pic'+ picNo++  : picNo=0, 'Pic'+picNo}, //ссылка на файл изображения
+                            productCost: 10*i, //цена
+                            productEnable: 1,
+                            productPromo: 1
+
+                        });
+
+                        
+
+
+                        prodCard.save()
+                            .then(function(doc){
+                                console.log("Сохранен объект", doc);
+
+                            })
+                            .catch(function (err){
+                                console.log('Ошибка!!!!!!!!!!!!!!!!!!!!!!!!!!   ',err);
+
+                            });
+                    }
+                }
+
+
+                ProductCards.find({ productName: /кофе/i  }).limit(10)
+                    .exec( function (err, small) {
+                        if (err){
+                            console.log('Erroo = ',err);
+                            return err;}
+                        else{
+                            res.status(200).send(small); //json
+                            console.log("Данные отправленны",small);
+                            //ProductCards.collection.drop();
+                        }
+                    });
+
+
+            }
+            //});
+
+            //console.log(ProductCardss);
+        });
+
+
+
+
+};
+// -- конец -- эта функция просто создает N карточек продуктов.....  техническая функция вне проекта
+
+// -- начало -- эта функция просто создает N карточек типов продуктов.....  техническая функция вне проекта
+async function get2minus(collect, res){
+    let docCount;
+    await ProductCards.find({}).exec(function(err, Product_Cards) {
+        console.log(Product_Cards);
+        Product_Cards.estimatedDocumentCount(function (err, count) {
+
+            if (err){
+                console.log(err)
+            }else{
+
+                docCount = count;
+                console.log("Estimated Count docCount= :", docCount);
+
+                if (docCount===0) {
+
+                    let prodType = new productType( {
+                        _id: new mongoose.Types.ObjectId(),
+                        productType: '0000000001',
+                        productName: 'Кофе',
+                        productTypeEnable: 0
+
+                    });
+
+                    prodType.save()
+                        .then(function(doc){
+                            console.log("Сохранен объект", doc);
+
+                        })
+                        .catch(function (err){
+                            console.log('Ошибка!!!!!!!!!!!!!!!!!!!!!!!!!!   ',err);
+
+                        });
+
+                }
+
+
+                /*
+                               Book.find({
+                                 title: /mvc/i
+                             }).sort('-created')
+                             .limit(5)
+                             .exec(function(err, books) {
+                                 if (err) throw err;
+
+                                 console.log(books);
+                             }); */
+                //.create
+                /*
+                                      _id: new mongoose.Types.ObjectId(),
+                                      coffeeName: `Какой-то кофе № ${docCount}`,
+                                      coffeeRegion: new mongoose.Types.ObjectId(),
+                                      detail: `Много много интересного`,
+                                      pic: "Картинка",
+                                      created:   new Date()
+                   */
+
+                ProductCards.find({
+                    productName: /кофе/i
+                }).limit(10)
+                    .exec( function (err, small) {
+                        if (err){
+                            console.log('Erroo = ',err);
+                            return err;}
+                        else{
+
+
+                            res.status(200).send(small); //json
+                            console.log("Данные отправленны",small);
+                            //ProductCards.collection.drop();
+
+                        }
+                    });
+
+
+            }
+        });
+        //console.log(ProductCardss);
+    });
+};
+// -- конец -- эта функция просто создает N карточек типов продуктов.....  техническая функция вне проекта
+
+
+// -- начало -- эта функция просто создает N карточек новостей.....  техническая функция вне проекта
+async function get2news(collect, res){
+    let docCount;
+    await News.find({}).exec(function(err, News_Cards) {
+        console.log(collect);
+        collect.estimatedDocumentCount(function (err, count) {
+
+            if (err){
+                console.log(err)
+            }else{
+
+                docCount = count;
+                console.log("Estimated Count docCount News= :", docCount);
+const fillText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+
+
+                if (docCount===0) {
+
+                    let newsCard;
+                    for (i=1;i<11;i++) {
+                        newsCard = new collect( {
+                            _id: new mongoose.Types.ObjectId(),
+                            newsTitle: "Новость № "+i,  //код типа продукта
+                            newsBody: i+") Много интересного "+fillText,
+                            newsEnable: 1,
+                            newsDateBegin: new Date()
+                            // newsDateEnd: Date
+
+                        });
+                        newsCard.save()
+                            .then(function(doc){
+                                console.log("Сохранен объект", doc);
+
+                            })
+                            .catch(function (err){
+                                console.log('Ошибка!!!!!!!!!!!!!!!!!!!!!!!!!!   ',err);
+
+                            });
+                    }
+                }
+
+
+
+                News.find({
+                    newsTitle: /кофе/i
+                }).limit(10)
+                    .exec( function (err, small) {
+                        if (err){
+                            console.log('Erroo = ',err);
+                            return err;}
+                        else{
+
+
+                            res.status(200).send(small); //json
+                            console.log("Данные отправленны",small);
+                            //ProductCards.collection.drop();
+
+                        }
+                    });
+
+
+            }
+        });
+        //console.log(ProductCardss);
+    });
+};
+// -- конец -- эта функция просто создает N карточек типов продуктов.....  техническая функция вне проекта
+
+
+
+
+
+module.exports.get2plus = get2plus;
+module.exports.get2minus = get2minus;
+module.exports.get2news = get2news
