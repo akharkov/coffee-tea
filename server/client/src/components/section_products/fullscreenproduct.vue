5c6906485c68a4f9a8ff27d5b93765b3dd5f223e
aKharkov <aKharkov@mail.ru>
2022-03-12 22:50:39 +0300

    Add table productSubTypes

--- server/client/src/components/section_products/fullscreenproduct.vue
+++ server/client/src/components/section_products/fullscreenproduct.vue
@@ -1,24 +1,30 @@
 
 
 <template>
+
+
+
      <transition name="fade">
           <div class="myModalBackground">
                <div class="myModalDiv">
-                    <div class="row">
-                         <div v-on:click="closeModal()"  style="float: left"> 
+               
+                    <div class="row" v-on:click="closeModal()"  style="float: left"> 
 X                         
-                         </div>  
-                         <div class="row" style="margin:0">
-                              <div class="col ">                            
-                                   
-                                   <img :src="this.picPath" alt="_А где картинка? :)_" >
-                            
-                                   {{this.$store.state.tmpProdObj }} 
+                    </div>  
+                    
 
-                              </div>
-                        
-                         </div>
+                    <div class="row productPic" style="margin:0">
+                         <img  :src="this.picPath" alt="_А где картинка? :)_" >
                     </div>
+
+                         
+                    
+
+                    <div >
+                         {{this.$store.state.tmpProdObj }} 
+                    </div>
+
+
 
                </div>
 
@@ -88,26 +94,6 @@ keys.forEach(key => {
                return myPath;
           }
 
-/* 
-          pathImage: function(){
-               const tmp=this.cardData.productType.productPicPath;
-               const fileName="I don.t now";
-               console.log(`this.cardData == ${tmp}`)
-               //this.cardData.productType.productPicPath; 
-               //this.$store.tmpProdObj.productType.productPicPath;
-     console.log(`nnn--${this.$store.tmpProdObj}`);
-     console.log(`this.cardData ${this.cardData}`);
-      //const fileName = `../../assets/images/coffee/pic2.jpg` //${this.Item.pic}
-      // const fileName = `../../assets/images/${this.Item.productType.productPicPath}/${this.Item.pic}` //${this.Item.pic}
-      //this.Item.pic; //.toLowerCase()
-               console.log('fileName==',fileName);
-               return tmp;
-               //return (`${fileName}`);
-      //return require(`..\\..\\img${this.Item.productType.productPicPath}${fileName}.jpg`);
-      //return `${fileName}`;
-               
-          }
-           */
 
      },
      mounted(){
@@ -120,6 +106,14 @@ keys.forEach(key => {
 
 <style scoped>
 
+/* styles for transitions */
+    .fade-enter-active, .fade-leave-active {
+    transition: opacity .8s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+    opacity: 0;
+    }
+/* styles for transitions */ 
      .myModalBackground{
         position: fixed;
         height: 100%;
@@ -131,34 +125,73 @@ keys.forEach(key => {
         z-index: 999999;
         
 
-        
-        /* top: 50%;   *//* position the top  edge of the element at the middle of the parent */
-        /* left: 50%;  *//* position the left edge of the element at the middle of the parent */
-
-        /* transform: translate(-50%, -50%); */ /* This is a shorthand of translateX(-50%) and translateY(-50%) */
-                                             
+                             
   
     }
      .myModalDiv{
-        position: fixed;
-        
+        position:  relative; 
+        margin: 5%;
+        padding: 5%;
+        max-width: 90%;
+        max-height: 95%;
         
         background-color: rgba(50, 112, 50, 0.4); 
 
-        
-        top: 50%;   /* position the top  edge of the element at the middle of the parent */
-        left: 50%;  /* position the left edge of the element at the middle of the parent */
+        align-items: center;
+        /* top: 50%; */   /* position the top  edge of the element at the middle of the parent */
+        /* left: 50%; */  /* position the left edge of the element at the middle of the parent */
 
-        transform: translate(-50%, -50%); /* This is a shorthand of translateX(-50%) and translateY(-50%) */
+        /* This is a shorthand of translateX(-50%) and translateY(-50%) */
          
+        /* transform: translate(-50%, -50%);  */
+       
         
         border: 2px solid  white;
         border-radius: 1em;
         -webkit-box-shadow: 10px 10px 10px 0px rgba(14, 49, 75, 0.4);
         -moz-box-shadow: 10px 10px 10px 0px rgba(14, 49, 75, 0.4);
         box-shadow: 10px 10px 10px 0px rgba(14, 49, 75, 0.4);
-        max-height: 95%;
+        
   
     }
+    .productPic{
+        /* background: rgba(253, 253, 253, 0); */
+        position:  absolute; 
+        left: 50%;  /* position the left edge of the element at the middle of the parent */
+
+        border-radius: 5%;
+        max-width: 100%;
+        height: 40%;
+        
+        /* margin: auto;
+        padding: 3%; */
+        /* width: max-content; */
+        /* object-position: 50% 50%; */
+        /* border: 8px double #FF0000;
+        border-top: 4px dotted #FDD201; */
+
+    }
+
+    .productPic img {
+        /* background: rgba(253, 253, 253, 0); */
+        border-radius: 5%;
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+        object-position: 50% 50%;
+
+        box-shadow: 5px 5px 5px rgba(116, 114, 114, 0.8);
+
+
+        /* border-radius: 50%; *//* 
+-webkit-box-shadow: inset 9px 9px 30px -12px rgba(0,0,0,0.7);
+-moz-box-shadow: inset 9px 9px 30px -12px rgba(0,0,0,0.7);
+box-shadow: inset 9px 9px 30px -12px rgba(0,0,0,0.7); */
+        }
+
+    
+
+
+
 
 </style>
